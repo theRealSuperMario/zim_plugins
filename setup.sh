7c40a0bb531efd975d7d97faed6d5d0df30183af
Sandro Braun <supermario94123@gmail.com>
2021-04-11 15:56:55 +0200

    Update setup.py

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+cd ~/.local/share/zim/plugins
 cp zim-plugin-instantsearch/instantsearch.py .
+cp zim-umldiagrameditor/umldiagrameditor.py .
+cp tag-autocompletion/tagcomplete.py .
+
+
+
+
+
+
